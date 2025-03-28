ed3d0e1c3c0353b8c0f31ad6e307d79b8a3ba4ed
karlo-kasi <karlokasi1995@gmail.com>
2025-03-28 18:28:42 +0100

    Add: Second setup

---
+++ front-end/src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage(){
+    return(
+        <>
+        <h2>Sono la Home</h2>
+        </>
+    )
+}
---
+++ front-end/src/pages/ProductPage.jsx
@@ -0,0 +1,7 @@
+export default function ProductPage(){
+    return(
+        <>
+        <h2>Sono ProductPage</h2>
+        </>
+    )
+}
