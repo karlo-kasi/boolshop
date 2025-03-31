6d6a62efaddaaa39eb1fc70761a292bc6e98b64c
karlo-kasi <karlokasi1995@gmail.com>
2025-03-31 10:35:20 +0200

    Add: checkout + checkoutpage

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,7 @@ import DefaultLayout from './layouts/DefaultLayout';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
+import CheckoutPage from './pages/CheckoutPage';
 
 function App() {
   return (
@@ -9,9 +10,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route Component={DefaultLayout}>
-            
+
             <Route path="/" Component={HomePage} />
             <Route path="/:id" Component={ProductPage} />
+            <Route path="/checkout" Component={CheckoutPage} />
           </Route>
         </Routes>
       </BrowserRouter>
