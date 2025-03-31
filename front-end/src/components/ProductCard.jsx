735b617362015145fc0653bd8b25b9f64e0338ec
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-03-31 11:03:19 +0200

    Add: ProductCard.jsx Link

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function ProductCard() {
   const [products, setProducts] = useState([]);
@@ -29,14 +30,14 @@ export default function ProductCard() {
       {products.map((p) => {
         return (
           <div className="col" key={p.id}>
-            <div className="card">
+            <Link to={`/${p.slug}`} className="card text-decoration-none">
               <img src={p.image} className="card-img-top" alt={p.name} />
               <div className="card-body">
                 <h5 className="card-title">{p.name}</h5>
                 <p className="card-text">{p.description}</p>
                 <p className="card-text">${p.price}</p>
               </div>
-            </div>
+            </Link>
           </div>
         );
       })}
