3716ab19ba8d01ed6b8b2aea195b6415abc6fc55
fabiodagostino27 <fabiodago27@gmail.com>
2025-03-31 09:52:23 +0200

    add cors

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,13 @@
 import express from "express"
 import router from "./routers/coverRouter.js"
 import setImagePath from "./middlewares/imagePath.js"
+import cors from cors
 const app = express()
 const port = 3000
 
+app.use(cors({
+    origin: "http://localhost:5173/"
+}));
 app.use(express.static("public"))
 app.use(express.json())
 app.use(setImagePath)
