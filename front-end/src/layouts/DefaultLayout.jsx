cbad4cedea34d9bf8f90b26316c4679873151ac2
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-03-31 18:31:54 +0200

    add header, hero, shippingBanner

--- front-end/src/layouts/DefaultLayout.jsx
+++ front-end/src/layouts/DefaultLayout.jsx
@@ -2,14 +2,12 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 import FreeShippingBanner from "../components/FreeShippingBanner";
-import Hero from "../components/Hero";
 
 export default function DefaultLayout() {
   return (
     <>
       <FreeShippingBanner />
       <Header />
-      <Hero />
       <main className="container">
         <Outlet />
       </main>
--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
+import Hero from "../components/Hero";
+
 export default function HomePage() {
   return (
     <>
-      <h2>Sono la Home</h2>
+      <Hero />
     </>
   );
 }
